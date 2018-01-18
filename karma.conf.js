b15f1f60c8f36003e9541d7a3507921968f96fb6
Nicolas Rod <ncls.rod@gmail.com>
2018-01-18 11:44:18 +0100

    Tried to add a rest named module

--- karma.conf.js
+++ karma.conf.js
@@ -20,24 +20,35 @@ module.exports = function (config) {
 			'src/bower_components/angular-sanitize/angular-sanitize.js',
 			'src/bower_components/angular-animate/angular-animate.js',
 			'src/bower_components/angular-touch/angular-touch.js',
-			'src/bower_components/angular-ui-router/release/angular-ui-router.js',
 			'src/bower_components/angular-bootstrap/ui-bootstrap-tpls.js',
+			'src/bower_components/angular-ui-router/release/angular-ui-router.js',
 			'src/bower_components/angular-aria/angular-aria.js',
-			'src/bower_components/angular-material/angular-material.js',
-			'src/bower_components/angular-messages/angular-messages.js',
-			'src/bower_components/angular-material-icons/angular-material-icons.js',
 			'app/app.js',
 			'app/modules/home/homeModule.js',
 			'app/modules/home/homeCtrl.js',
 			'app/modules/home/homeRoute.js',
 			'app/modules/home/homeService.js',
-			'app/modules/home/home-test.js'
+			'app/modules/home/home-test.js',
+			'app/modules/rest/restModule.js',
+			'app/modules/rest/restCtrl.js',
+			'app/modules/rest/restRoute.js',
+			'app/modules/rest/restService.js',
+			'app/modules/rest/rest-test.js',
 		],
 
 		// Test results reporter to use
 		// Possible values: 'dots', 'progress', 'junit', 'growl', 'coverage'
 		//reporters: ['progress'],
 		reporters: ['spec'],
+
+		plugins : [
+			'karma-jasmine',
+			'karma-coverage',
+			'karma-chrome-launcher',
+			'karma-firefox-launcher',
+			'karma-phantomjs-launcher',
+			'karma-spec-reporter'
+		],
 
 		// Web server port
 		port: 9876,
